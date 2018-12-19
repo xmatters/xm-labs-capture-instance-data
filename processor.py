cd0e3f7ce0c5e0ff145a659e62a29eb7da623dd0
Jordan Olin <jolin@xmatters.com>
2018-12-18 22:02:59 -0700

    Added capture of Groups and Shifts

--- processor.py
+++ processor.py
@@ -250,6 +250,84 @@ def _process_users(include_devices: bool):
     users_file.write(']')
     users_file.close()
 
+def _get_group(group_id: str, target_name: str):
+    """Attempst to retrieve Group by id.
+        
+        If the Group exists, retrieve and return the object.
+        If not, return None
+        
+        Args:
+        group_id (str): UUID of Group to retrieve
+        target_name (str): targetName field value for the specified Group.
+        """
+    _logger.debug(f"Retrieving Group: {target_name}")
+    
+    # Set our resource URLs
+    url = config.xmod_url + '/api/xm/1/groups/' + urllib.parse.quote(group_id) + '?embed=supervisors'
+    _logger.debug(f'Attempting to retrieve Group "{target_name}" via url: {url}')
+    
+    # Make the request
+    try:
+        response = requests.get(url, auth=config.basic_auth)
+    except requests.exceptions.RequestException as e:
+        _logger.error(config.ERR_REQUEST_EXCEPTION_CODE, url, repr(e))
+        return None
+    
+    # If the initial response fails, log and return null
+    if response.status_code != 200:
+        _log_xm_error(url, response)
+        return None
+    
+    # Process the response
+    group_obj = response.json()
+    # _logger.debug(f'Found Group "{group_obj["targetName"]}" - json body: {pprint.pformat(group_obj)}')
+    _logger.debug(f'Found Group "{group_obj["targetName"]}" - json body.id: {group_obj["id"]}')
+    return group_obj
+
+def _get_group_shifts(group_id: str, target_name: str):
+    """Return a Group's Shifts
+
+    Retrieves the Shift records from xMatters for the specified Group and
+    returns them as a list.
+
+    Args:
+        group_id (str): The Group's UUID
+        target_name (str): The Group's targetName field
+
+    Return:
+        shift_list (list): List of dictionaries of the Group's Shifts.
+    """
+    # Initialize conditions
+    shift_list = []
+    total_shifts = 0
+    url = config.xmod_url + '/api/xm/1/groups/' + group_id + '/shifts/?embed=members&offset=0&limit=' + str(config.page_size)
+    _logger.debug(f'Gathering Shifts for Group "{target_name}", url={url}')
+
+    while True:
+
+        # Get the site records
+        response = requests.get(url, auth=config.basic_auth)
+        if response.status_code not in [200, 404]:
+            _log_xm_error(url, response)
+            break
+
+        # Process the responses
+        bodys = response.json()
+        total_shifts = bodys['total']
+        if bodys['count'] > 0:
+            _logger.debug('%d Count of %d Total Shifts found for Group "%s" via url=%s', bodys['count'], bodys['total'], target_name, url)
+            shift_list += bodys['data']
+
+        # See if there are any more to get
+        if 'links' in bodys and 'next' in bodys['links']:
+            url = config.xmod_url + bodys['links']['next']
+        else:
+            break
+
+    _logger.debug(f'Collected {len(shift_list)} of a possible {total_shifts} Shifts for Group "{target_name}".')
+
+    return shift_list
+
 def _process_groups():
     """Capture and save the instances Group objects
 
@@ -262,7 +340,57 @@ def _process_groups():
     Return:
         None
     """
-    pass
+    groups_file = _create_out_file(config.groups_filename)
+    groups_file.write('[\n')
+
+    # Initialize conditions
+    group_objects = []
+    total_groups = 0
+    cnt = 0
+    url = config.xmod_url + '/api/xm/1/groups?offset=0&limit=' + str(config.page_size)
+    _logger.debug('Gathering Groups, url=%s', url)
+
+    while True:
+
+        # Get the group records
+        response = requests.get(url, auth=config.basic_auth)
+        if response.status_code not in [200, 404]:
+            _log_xm_error(url, response)
+            break
+
+        # Process the responses
+        bodys = response.json()
+        total_groups = bodys['total']
+        if bodys['count'] > 0:
+            _logger.debug("%d Count of %d Total Groups found via url=%s", bodys['count'], bodys['total'], url)
+            for body in bodys['data']:
+
+                # Get the full Group object, including Roles and Supervisors
+                group_obj = {}
+                a_group = _get_group(body['id'], body['targetName'])
+                if a_group is not None:
+                    group_obj['group'] = a_group
+
+                    # Get the shifts,
+                    group_obj['shifts'] = _get_group_shifts(a_group['id'], a_group['targetName'])
+
+                    # Save the Group
+                    cnt += 1
+                    json.dump(group_obj, groups_file)
+                    groups_file.write(',\n') if cnt < total_groups else groups_file.write('\n')
+
+            group_objects += bodys['data']
+
+        # See if there are any more to get
+        if 'links' in bodys and 'next' in bodys['links']:
+            url = config.xmod_url + bodys['links']['next']
+        else:
+            break
+            
+    _logger.debug(f"Collected {len(group_objects)} of a possible {total_groups} Groups.")
+
+    groups_file.write(']')
+    groups_file.close()
 
 def process(objects_to_process: list):
     """Capture objects for this instance.
